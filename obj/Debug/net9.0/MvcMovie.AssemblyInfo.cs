c835b9b9a3fb403a52fc0efa814bc35f6505b346
Erick Moura <erick.andrew.moura@gmail.com>
2025-11-12 02:02:04 -0300

    "Part 7 - Completed"

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e241fa9b8fc8fab17bf922355b268bfe42bea3f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76849a30fbde62256b338b098e43e931a55e94f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
